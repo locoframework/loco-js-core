480f94e22a1c1bc973f5544b81b019701bf2f2bf
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-02-21 23:27:26 +0100

    [test]

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -1,24 +1,36 @@
 import { init, Controllers } from "index";
 
-let working;
+let counter;
 
-class Main extends Controllers.Base {
-  list() {
-    working = true;
+class Wrapper extends Controllers.Base {
+  initialize() {
+    counter += 1;
   }
 }
 
-Object.assign(Controllers, { Main });
+class Main extends Controllers.Base {
+  initialize(){
+    counter += 2;
+  }
+
+  list() {
+    counter += 3;
+  }
+}
+
+Object.assign(Wrapper, { Main });
+Object.assign(Controllers, { Wrapper });
 
 beforeEach(() => {
-  working = false;
+  counter = 0;
 });
 
 it("calls out a correct controller action based on body attributes", () => {
+  document.body.setAttribute('data-namespace', "Wrapper");
   document.body.setAttribute('data-controller', "Main");
   document.body.setAttribute('data-action', "list");
 
-  expect(working).toBe(false);
+  expect(counter).toEqual(0);
   init(Controllers);
-  expect(working).toBe(true);
+  expect(counter).toEqual(6);
 });
